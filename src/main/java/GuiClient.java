e0675c44fa3ca5011578b69a2bfb2ca33b1a6632
age30s <adithprasad@gmail.com>
2024-04-15 01:08:59 -0500

    login screen

--- src/main/java/GuiClient.java
+++ src/main/java/GuiClient.java
@@ -5,14 +5,18 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -69,11 +73,59 @@ public class GuiClient extends Application{
 
 	
 	public Scene createClientGui() {
-		
-		clientBox = new VBox(10, c1,b1,listItems2);
-		clientBox.setStyle("-fx-background-color: blue;"+"-fx-font-family: 'serif';");
-		return new Scene(clientBox, 400, 300);
-		
+
+		return loginScreen();
+	}
+
+	public Scene loginScreen(){
+
+		BorderPane borderPane = new BorderPane();
+		Button loginButton = new Button("Login");
+		loginButton.setStyle("-fx-font-size:20");
+
+		loginButton.setPrefWidth(100);
+		loginButton.setPrefHeight(40);
+
+		loginButton.setStyle(
+				"-fx-background-radius: 5em; ");
+
+		Label welcomelabel = new Label("Welcome to UIchat!");
+		welcomelabel.setFont(Font.font("Georgia",50));
+		welcomelabel.setStyle("-fx-padding: 50 0 0 0;");
+
+		TextField user = new TextField("Username");
+		TextField pass = new TextField("Password");
+
+		user.maxWidth(500);
+		pass.maxWidth(500);
+
+		user.minWidth(100);
+		pass.minWidth(100);
+
+		VBox vbox = new VBox(user,pass,loginButton);
+		user.setAlignment(Pos.CENTER);
+		pass.setAlignment(Pos.CENTER);
+		loginButton.setAlignment(Pos.CENTER);
+
+
+		vbox.setSpacing(20);
+		borderPane.setCenter(vbox);
+		borderPane.setTop(welcomelabel);
+		BorderPane.setAlignment(vbox, Pos.CENTER);
+
+		// this is what sends the messages
+		// will be deleted in the future
+		// uncomment to see
+//		clientBox = new VBox(10, c1,b1,listItems2);
+//		borderPane.setBottom(clientBox);
+
+//
+		borderPane.setStyle("-fx-background-color: #b9dbf6;");
+
+
+		Scene scene = new Scene(borderPane, 700,700);
+
+		return scene;
 	}
 
 }
